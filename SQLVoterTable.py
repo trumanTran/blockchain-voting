920a05ef3cc312f432bbb085035b5b19c662f7b6
johnl11235 <31864579+johnl11235@users.noreply.github.com>
2017-11-27 11:33:18 -0500

    Added CSV loading option
    Added a functionality to load an entire CSV file that defines voters as whole name, address tuples.

--- SQLVoterTable.py
+++ SQLVoterTable.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 import sys
-
+import csv
 
 #The goal is to create three entries on the table: The name of the person (hashed), the address of the person (also hashed), and a bool value that checks if they're registered or not.
 #For the hash, a cryptographic hash would be optimal.
@@ -22,14 +22,22 @@ def insert_voter(voter_name, voter_add):
 	cur.execute('INSERT INTO voter_reg VALUES (?,?,?)', (voter_name, voter_add, '0'))
 	#Each voter must be inserted manually, but there will be a bulk insertion method avaliable later when this is refined.
 
-def mass_insert_voters(voter_name_list, voter_add_list):
+def mass_insert_voters(voter_list):
 	#Inserts n amount of names and addresses.
-	if(len(voter_name_list) == len(voter_add_list)):
-		for i in range(0, len(voter_name_list)):
-			insert_voter(voter_name_list[i], voter_add_list[i])
-		#Stuff inserted.
-	else:
-		print "Bad Input."
+    for i in range(0, len(voter_list)):
+		insert_voter(voter_list[i][0], voter_list[i][0])
+	#Stuff inserted.
+	
+def CSV_Load_Voters(file_name):
+	#Function that takes the name of a file and loads it as a list of tuples. This list is then used to insert via the mass insert voters function.
+	'''file must be in the following format for voter insertion:
+	whole name, address
+	Takes the result as a tuple, which is then processed by mass_insert_voters.'''
+	with open(file_name, 'r') as file:
+		reader = csv.reader(file)
+		v_list = list(reader) #Entire list loaded into voter_list as a list of tuples.
+		#Now that we are done opening the file, do things.
+		mass_insert_voters(v_list) #Voters inserted.
 	
 def check_voter(target_name, target_address):
 	'''Simplified version of hit_entry that has predefined arguments.
@@ -43,9 +51,12 @@ def check_voter(target_name, target_address):
 		#Pass an argument that sets the bool column to true, and also confirm the voter can vote.
 		'''Insert vote message passing thing here.'''
 		cur.execute('UPDATE voter_reg SET hasvoted = 1 WHERE name=? AND address=?', (target_name, target_address)) #Execute bool change.
+		return True;
 	elif(bool_val == '1'):
 		#They voted already.
 		'''Insert already voted message.'''
+		return False;
 	else():
 		#No hits on list, suspicious...
 		'''Insert not registered voter message.'''
+		return False;
