4d778dded44335e2e235bde9458b478ea3c858c0
Aaron <aaronpng@gmail.com>
2017-11-29 14:30:10 -0500

    Update gui2.py

--- dynamicGUI/gui2.py
+++ dynamicGUI/gui2.py
@@ -4,23 +4,28 @@ import gui_header
 import example_container
 ballot = gui_header.LoadCSV('Ballot.csv')
 votes = []
-WriteIns = []
+WriteInEntries = []
 limitations = []
 all_frames = []
 compiledBallot = []
 frameCount = 1
 block = example_container.Vote
-# ---------- Root Window declaration ----------
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------- Root Window declaration ----------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 root = Tk()
-#root.attributes("-fullscreen", True)
+root.attributes("-fullscreen", True)
 root.title("Voting Booth")
-root.config(width = 600, height = 600)
-
-# ---------- create first frame, confirmation page, last frame ----------
+#root.config(width = 600, height = 600)
+# ---------------------------------------------------------------------------------------------------------------------
+# -------------------------------- create first frame, confirmation page, last frame ----------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 LoginFrame = Frame(root)
 confirmation_page = Frame(root)
 lastFrame = Frame(root)
-# ---------- Local function definaitions ----------
+# ---------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------- Local function definaitions --------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 def proceed():
     global frameCount
     if frameCount < len(all_frames) - 1:
@@ -35,30 +40,57 @@ def lessCheck(counter):
         answer = tkinter.messagebox.askquestion("YOU ARE ABOUT TO OMIT YOUR VOTE(S)",
                                                 "Are you sure you would like to omit your vote(s) for this race?")
         if answer == 'yes':
-            WriteIns[len(WriteIns) - frameCount + 1].config(state='disabled')
+            WriteInEntries[len(WriteInEntries) - frameCount + 1].config(state='disabled')
             proceed()
     else:
-        WriteIns[len(WriteIns) - frameCount + 1].config(state='disabled')
+        WriteInEntries[len(WriteInEntries) - frameCount + 1].config(state='disabled')
         proceed()
 
-def check():
+def populate():
+    global frameCount
+    for i in range(0, len(votes)):
+        if limitations[i][0] > 1:
+            counter = 0
+            for j in range(len(votes[i])):
+                if votes[i][len(votes[i]) - 1].get() == 1 and counter == 0:
+                    compiledBallot[i].append(WriteInEntries[len(WriteInEntries) - 1 - i].get())
+                    counter += 1
+                elif votes[i][j].get() == 1 and j < len(votes[i])-1 :
+                    compiledBallot[i].append(ballot[i][j + 2])
+                    counter += 1
+            while counter < limitations[i][0]:
+                counter += 1
+                compiledBallot[i].append('Omitted')
+        else:
+            if votes[i][0].get() > 0:
+                if votes[i][0].get() == limitations[i][1]:
+                    compiledBallot[i].append(WriteInEntries[len(WriteInEntries) - 1 - i].get())
+                else:
+                    compiledBallot[i].append(ballot[i][votes[i][0].get() + 1])
+            else:
+                compiledBallot[i].append('Omitted')
+    #print(compiledBallot)  # send instance of John's container to the back end
+    block.set_votes(block, compiledBallot)
+    block.set_id(block, nameInfo.get() + addressInfo.get())
+
+def check(confirmation):
     global frameCount
     if limitations[frameCount - 2][0] == 1:
         if votes[frameCount - 2][0].get() == 0:
             answer = tkinter.messagebox.askquestion("YOU ARE ABOUT TO OMIT YOUR VOTE",
                                                     "Are you sure you would like to omit your vote for this race?")
             if answer == 'yes':
-                WriteIns[len(WriteIns) - frameCount + 1].config(state='disabled')
+                WriteInEntries[len(WriteInEntries) - frameCount + 1].config(state='disabled')
                 proceed()
         else:
             if votes[frameCount - 2][0].get() == limitations[frameCount - 2][1]:
-                if (WriteIns[len(WriteIns) - frameCount + 1].get()).strip() == '':
+                if (WriteInEntries[len(WriteInEntries) - frameCount + 1].get()).strip() == '':
                     tkinter.messagebox.showinfo("NOTICE", "The Write-In ballot has been left blank")
                 else:
-                    WriteIns[len(WriteIns) - frameCount + 1].config(state='disabled')
+                    WriteInEntries[len(WriteInEntries) - frameCount + 1].config(state='disabled')
                     proceed()
             else:
-                WriteIns[len(WriteIns) - frameCount + 1].config(state='disabled')
+                WriteInEntries[len(WriteInEntries) - frameCount + 1].config(state='disabled')
                 proceed()
     else:
         counter = 0
@@ -69,69 +101,58 @@ def check():
             tkinter.messagebox.showinfo("NOTICE", "You have voted for more candidates than you are allowed to")
 
         elif (votes[frameCount - 2][limitations[frameCount - 2][1] - 1].get() == 1):
-            if (WriteIns[len(WriteIns) - frameCount + 1].get()).strip() == '':
+            if (WriteInEntries[len(WriteInEntries) - frameCount + 1].get()).strip() == '':
                 tkinter.messagebox.showinfo("NOTICE", "The Write-In ballot has been left blank")
             else:
                 lessCheck(counter)
         else:
             lessCheck(counter)
-
-def populate():
-    for i in range(0, len(votes)):
-        if limitations[i][0] > 1:
-            counter = 0
-            for j in range(len(votes[i])):
-                if votes[i][len(votes[i]) - 1].get() == 1 and counter == 0:
-                    compiledBallot[i].append(WriteIns[len(WriteIns) - 1 - i].get())
-                    counter += 1
-                elif votes[i][j].get() == 1 and j < len(votes[i])-1 :
-                    compiledBallot[i].append(ballot[i][j + 2])
-                    counter += 1
-            while counter < limitations[i][0]:
-                counter += 1
-                compiledBallot[i].append('Omit')
-        else:
-            if votes[i][0].get() > 0:
-                if votes[i][0].get() == limitations[i][1]:
-                    compiledBallot[i].append(WriteIns[len(WriteIns) - 1 - i].get())
-                else:
-                    compiledBallot[i].append(ballot[i][votes[i][0].get() + 1])
-            else:
-                compiledBallot[i].append('Omit')
-    #print(compiledBallot)  # send instance of John's container to the back end
-    block.set_votes(block, compiledBallot)
-    block.set_id(block, nameInfo.get() + addressInfo.get())
-    block.print_votes(block)
+    if confirmation == 1:
+        populate()
+        gui_header.fillConfirmation(0, compiledBallot, canvasFrame)
 
 def reset():
     global frameCount
-    populate()
     LoginFrame.tkraise()
     frameCount = 1
     for i in range(0, len(votes)):
         for j in range(len(votes[i])):
             votes[i][j].set(0)
-    for k in range(0,len(WriteIns)):
-        WriteIns[k].delete(0,END)
+    for entry in WriteInEntries:
         var = IntVar(0)
-        gui_header.enable(WriteIns[k],var,1)
+        gui_header.enable(entry, var, -1)
+        entry.delete(0,END)
+        gui_header.enable(entry,var,1)
     nameInfo.delete(0,END)
     addressInfo.delete(0,END)
     for i in compiledBallot:
         for j in range(1,len(i)):
             del i[1]
-# -------------------- login Frame --------------------
+
+def configureCanvas(event):
+    canvas.configure(scrollregion=canvas.bbox("all"), width=500, height=500)
+
+def changeVote():
+    global frameCount
+    frameCount = 1
+    for i in compiledBallot:
+        for j in range(1,len(i)):
+            del i[1]
+    proceed()
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------- login Frame ----------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 LoginFrame_subFrame = Frame(LoginFrame)
 LoginFrame_subFrame.grid(column=1, row=1)
 # ----- name and address labels -----
-name = Label(LoginFrame_subFrame, text="NAME")
-address = Label(LoginFrame_subFrame, text="ADDRESS")
-welcome = Label(LoginFrame, text="WELCOME")
+name = Label(LoginFrame_subFrame, text="NAME", font=("",12))
+address = Label(LoginFrame_subFrame, text="ADDRESS", font=("",12))
+welcome = Label(LoginFrame, text="WELCOME", font=("",18))
 # ----- entry boxes -----
-nameInfo = Entry(LoginFrame_subFrame)
-addressInfo = Entry(LoginFrame_subFrame)
+nameInfo = Entry(LoginFrame_subFrame, font=("",12))
+addressInfo = Entry(LoginFrame_subFrame, font=("",12))
 # ----- button definition -----
-submit = Button(LoginFrame, text="SUBMIT", command = proceed)
+submit = Button(LoginFrame, text="SUBMIT", command = proceed, font=("",14))
 submit.config(width=30, bg='gray80')
 quit = Button(LoginFrame, text="QUIT", command= lambda x = root: gui_header.quitCheck(x))
 # ----- placement of widgets onto the first frame -----
@@ -142,25 +163,44 @@ nameInfo.grid(row=0, column=1, sticky=EW)
 addressInfo.grid(row=1, column=1, sticky=EW)
 submit.grid(row=1, column=1, sticky=S)
 quit.grid(row=2, column=1, sticky=W)
-# -------------------- Last frame --------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------- Last frame ----------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 lastFrame_SubFrame = Frame(lastFrame)
 lastFrame_SubFrame.grid(column=1, row=1)
-thanks = Label(lastFrame_SubFrame, text="THANK YOU FOR VOTING")
-end = Button(lastFrame_SubFrame, text="FINISH", command = reset)
-# -------------------- Confirmation page --------------------
+thanks = Label(lastFrame_SubFrame, text="THANK YOU FOR VOTING", font=("",14))
+end = Button(lastFrame_SubFrame, text="FINISH", command = reset, font=("",14))
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------- Confirmation page -------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 confirmation_subframe = Frame(confirmation_page)
-confirmation_subframe.grid(column = 1, row = 1)
-confirm = Button(confirmation_subframe, text = 'confirm', command = proceed)
-prompt = Label(confirmation_subframe, text = 'PLEASE CONFIRM')
-# -------------------- Placement of wigits --------------------
-for i in [thanks, end, prompt, confirm]:
+confirmation_subframe.grid(column=1, row=1)
+confirm = Button(confirmation_subframe, text='CONFIRM', command=proceed)
+redo = Button(confirmation_subframe, text='CHANGE', command=changeVote)
+prompt = Label(confirmation_subframe, text='PLEASE CONFIRM', font=("",14))
+# ----- Scrollbar in case the entries in a ballot extend off screen -----
+myscrollbar=Scrollbar(confirmation_subframe,orient="vertical")
+canvas=Canvas(confirmation_subframe)
+canvas.configure(yscrollcommand=myscrollbar.set)
+canvasFrame=Frame(canvas)
+myscrollbar.config(command=canvas.yview)
+myscrollbar.pack(side="right",fill="y")
+canvas.create_window((250,0),window=canvasFrame,anchor='n')
+canvas.bind("<Configure>",configureCanvas)
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------- Placement of wigits and frames ------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+for i in [thanks, end, prompt, canvas]:
     i.pack()
-# -------------------- Placement of frames --------------------
+confirm.pack(side='right')
+redo.pack(side='left')
 for i in [LoginFrame, confirmation_page, lastFrame]:
     gui_header.configure(i)
     i.place(relx=0.5, rely=0.5, anchor="c", relwidth=1.0, relheight=1.0)  # add new frame to the window
     all_frames.append(i)
-# -------------------- Dynamically create ballot based off ballot tuple --------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------- Dynamically create ballot based off ballot tuple ---------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 for i in range(0,len(ballot)):
     ballot_entry = Frame(root) # frame for all information regarding one race
     gui_header.configure(ballot_entry)
@@ -169,57 +209,67 @@ for i in range(0,len(ballot)):
     subFrame = Frame(ballot_entry) # subframe for the general information to go
     candidateFrame = Frame(subFrame) # subfframe of subframe for the specific candidate entries
     subFrame.grid(row = 1, column = 1)
-    raceText = Label(subFrame, text = ballot[i][0])
+    raceText = Label(subFrame, text = ballot[i][0], font=("",14))
     compiledBallot.append([ballot[i][0]])
-    nextButton = Button(subFrame, text = 'NEXT', command = check)
-    directionsText = Label(subFrame, text="Please vote for " + (str)(ballot[i][1]))
+    nextButton = Button(subFrame, text = 'NEXT', command = lambda x = 0: check(x))
+    if i == 0:
+        nextButton.config(command = lambda x = 1: check(x))
+    directionsText = Label(subFrame, text="Please vote for " + (str)(ballot[i][1]), font=("",10))
     for iterator in (raceText, directionsText, candidateFrame, nextButton):
         iterator.pack()
     limit = [0, 0] # Allowed vote, number of candidates
+    writeIn = Entry(candidateFrame, state='disabled', font=("", 12))
+    WriteInEntries.append(writeIn)
     if ballot[i][1] == '1': # if a single candidate race
         limit[0] = 1
         var = IntVar()
         votes.append([var])
-        omit = Radiobutton(candidateFrame, text = 'Omit', variable = votes[i][0], value = 0)
+        omit = Radiobutton(candidateFrame, text = 'Omit', variable = votes[i][0], value = 0, font=("",12))
         omit.grid(sticky = 'W', row = 0)
-        wrb = Radiobutton(candidateFrame, variable=votes[i][0])
+        wrb = Radiobutton(candidateFrame, variable=votes[i][0], font=("",12))
         wrb.grid(sticky='W', row=len(ballot[i]))
-        writeIn = Entry(candidateFrame, state='disabled')
-        writeIn.grid(sticky='W', row=len(ballot[i]), padx=30)
-        WriteIns.append(writeIn)
+        writeIn.grid(sticky='W', row=len(ballot[i]), padx=28)
         for j in range (2, len(ballot[i])):
             if ballot[i][j] != '':
                 limit[1]+=1
-                rb = Radiobutton(candidateFrame, text = ballot[i][j], variable = votes[i][0], value = limit[1],
-                                 command=lambda e=WriteIns[i], v=votes[i][0], x = len(ballot[i]) - 2:
+                rb = Radiobutton(candidateFrame, text = ballot[i][j], variable = votes[i][0], value = limit[1], font=("",12),
+                                 command=lambda e=WriteInEntries[i], v=votes[i][0], x = len(ballot[i]) - 2:
                                  gui_header.enable(e, v, x))
                 rb.grid(sticky = 'W', row = j-1)
-        wrb.configure(value=limit[1] + 1, command=lambda e=WriteIns[i], v=votes[i][0], x = limit[1]:
+        wrb.configure(value=limit[1] + 1, command=lambda e=WriteInEntries[i], v=votes[i][0], x = limit[1]:
                                          gui_header.enable(e, v, x))
         limit[1]+=1
-    else: # if multiple candidates may be selected
+    else: # if multiple candidates can be selected
         limit[0] = ((int)(ballot[i][1]))
         candidateArray = []
         for j in range (2, len(ballot[i])):
             if(ballot[i][j] != ''):
                 var = IntVar()
                 candidateArray.append(var)
-                cb = Checkbutton(candidateFrame, text = ballot[i][j], variable = candidateArray[j-2])
+                cb = Checkbutton(candidateFrame, text = ballot[i][j], variable = candidateArray[j-2], font=("",12))
                 cb.grid(sticky = 'W', row = j - 2)
                 limit[1]+=1
         # --- Write-in ---
         var = IntVar()
         candidateArray.append(var)
-        writeIn = Entry(candidateFrame, state='disabled')
-        WriteIns.append(writeIn)
-        writeIn.grid(sticky='W', row=len(ballot[i]) - 1, padx=30)
-        cb = Checkbutton(candidateFrame, variable = candidateArray[len(candidateArray)-1],
-                         command=lambda e=WriteIns[i], v=candidateArray[len(candidateArray)-1], x=0:
+        writeIn.grid(sticky='W', row=len(ballot[i]) - 1, padx=28)
+        cb = Checkbutton(candidateFrame, variable = candidateArray[len(candidateArray)-1], font=("",12),
+                         command=lambda e=WriteInEntries[i], v=candidateArray[len(candidateArray)-1], x=0:
                          gui_header.enable(e, v, x))
         limit[1]+=1
         cb.grid(sticky = 'W', row = len(ballot[i]) - 1)
         votes.append(candidateArray)
     limitations.append(limit)
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------ Populate confirmation list with blank space ------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+listRowNumber = len(ballot)
+for i in range(len(limitations)):
+    listRowNumber = listRowNumber + limitations[i][0]
+gui_header.fillConfirmation(listRowNumber, [], canvasFrame)
+# ---------------------------------------------------------------------------------------------------------------------
+# -------------------------------------- Flip around lists to match output of GUI -------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 limitations.reverse()
 votes.reverse()
 ballot.reverse()
