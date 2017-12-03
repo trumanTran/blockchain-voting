191d613dd319783259901c75bb78204c86f2dbd5
johnl11235 <31864579+johnl11235@users.noreply.github.com>
2017-12-03 09:37:47 -0500

    Added commit changes to ensure it commits changes.
    Explained in title.

--- SQLKeyTable.py
+++ SQLKeyTable.py
@@ -19,6 +19,7 @@ def create_key_table():
 def insert_to_key_table(sig, ip_add, pub_key):
 	#Insert to the key table the values above.
 	cur.execute('INSERT INTO key_table VALUES (?,?,?)', (sig, ip_add, pub_key))
+	con.commit()
 
 def ip_change(sig, new_ip):
 	#Take a signature, find a hit on the SQL table, and then change its corresponding IP value to the new value.
@@ -35,11 +36,12 @@ def delete_entry(sig):
 	#Function to select a node's entries by signature, for node removal.
 	cur.execute('DELETE FROM key_table WHERE sig=?', (sig,))
 	#Deletes the node's entry. Since the node's actual entry does not exist anymore, it functionally stops them from sending useful data or having data sent to them.
+	con.commit()
 
 def find_node_ips():
 	#This is for the broadcast function. It recalls ALL IPS CURRENTLY LISTED and returns the entire column of ips as a list of tuples. Access as if it were a list, since the column is literally one width wide.
 	cur.execute('SELECT ip_add FROM key_table')
 	return cur.fetchall()
-  
+
 """Table Creation"""
 create_key_table()
